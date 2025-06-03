ce8ce961c1795791170bf4aa1c64252c8df26981
DimoniumAnonimo <113396272+DimoniumAnonimo@users.noreply.github.com>
2025-06-02 20:05:02 -0500

    debug: test response code
    transmitter is always getting a prompt of 0 anyway, just fake it to see if the rest of the code works.

--- RadioReceiver/RadioReceiver.ino
+++ RadioReceiver/RadioReceiver.ino
@@ -153,9 +153,9 @@ static bool valid_request()
 
 static void set_prompt_and_response()
 {
-  prompt = millis();
   expected_response = 0xFFFFFFFF - prompt;
   //expected_response = calculate_response(prompt);
+  prompt = 0;
   Serial.print("prompt: ");
   Serial.print(prompt);
   Serial.print(" expected response: ");
